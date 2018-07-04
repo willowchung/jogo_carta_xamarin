b07fd81e092775cc64fe451892ee947a9e1d9453
Willow Chung <willow.chung@radixeng.com.br>
2018-07-03 21:14:58 -0300

    Adicionado gesto de toque nas imagens

--- JogoCartas/MainPage.xaml.cs
+++ JogoCartas/MainPage.xaml.cs
@@ -9,9 +9,29 @@ namespace JogoCartas
 {
     public partial class MainPage : ContentPage
     {
+        private static String jokerAsset = "joker_card.jpg";
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        async void OnTapCard(object sender, EventArgs args)
+        {
+            ChangeImageTo((Image)sender, jokerAsset);
+        }
+
+        async void ChangeImageTo(Image image, String asset)
+        {
+            await Task.WhenAll(
+                image.RotateYTo(90)
+            );
+
+            image.Source = asset;
+
+            await Task.WhenAll(
+                image.RotateYTo(360)
+            );
         }
     }
 }
